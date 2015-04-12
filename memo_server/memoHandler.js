4f4530424479822705a1ae3f3f7f666245e4a988
YUN YOUNGGI <YG@YUNui-MacBook-Pro.local>
2015-04-13 04:41:48 +0900

    6주차 과제

--- memo_server/memoHandler.js
+++ memo_server/memoHandler.js
@@ -1,70 +1,152 @@
-/** memoHandler.js **/
+/* memoHandler.js */
 
-var Datastore = require('nedb'); 
-var db = new Datastore({ filename: './data/memo', autoload: true }); 
-var querystring = require('querystring'); 
-var url = require('url'); 
- 
- 
-exports.create = function(req, res, body) { 
-	_insertMemo( body , function(error, result) { 
-		res.writeHead(200, {"Content-Type": "application/json"}); 
-		res.write('creatememo'); 
-		res.end(); 
-	}); 
-}; 
- 
-exports.read = function(req, res) { 
-	_findMemo({}, function(error, results) { 
-		res.writeHead(200, {"Content-Type": "application/json"}); 
-		res.write(JSON.stringify(results)); 
-		res.end(); 
-	}); 
-}; 
- 
-exports.update = function(req, res, body) { 
-	var query = url.parse(req.url).query; 
-	var where = querystring.parse(query); 
+var MongoDb = require("mongodb"),
+	MongoServer = new MongoDb.Server("localhost", 27017, {}),
+	db = new MongoDb.Db("users", MongoServer),
+	collection = db.collection("memo"),
+	url = require("url"),
+	fs = require("fs"),
+	querystring = require("querystring"),
+    formidable = require('formidable');
+	
+var UPLOAD_FOLDER = "./upload/";
 
-	_updateMemo(where, body, function(error, results) { 
-		res.writeHead(200, {"Content-Type": "application/json"}); 
-		res.write('updatememo'); 
-		res.end(); 
- 	}); 
-}; 
- 
-exports.remove = function(req, res, body) { 
-	var query = url.parse(req.url).query; 
-	var where = querystring.parse(query); 
- 	
-	_removeMemo(where, function(error, results) { 
-		res.writeHead(200, {"Content-Type": "application/json"}); 
-		res.write('removememo'); 
-		res.end(); 
-	}); 
-}; 
- 
-function _insertMemo(body , callback) { 
-	body = typeof body === 'string' ? JSON.parse(body) : body; 
 
-	var memo = { 
-		author: body.author, 
-		memo : body.memo, 
-		date: new Date() 
-	};   
+exports.create = function(req, res, body) {
 
-	db.insert(memo, callback); 
-};   
- 
-function _findMemo(where, callback) { 
-	where = where || {}; 
-	db.find(where, callback); 
-} 
- 
-function _updateMemo(where, body, callback) { 
-	db.update(where, {$set: body}, {multi: true}, callback); 
-} 
- 
-function _removeMemo(where, callback) { 
-	db.remove(where, {multi: true}, callback);
+	var files = [],
+    	fields = [],
+    	resultPaths = [],
+    	content = {};
+
+	console.log("upload!");
+	body.uploadDir = UPLOAD_FOLDER;
+	body.keepExtensions = true;
+	body.multiple="multiple";
+	
+	body.on('field', function(field, value) {
+	        fields.push([field, value]);
+	    })
+	    .on('file', function(field, file) {
+	        files.push([field, file]);
+	    })
+    	.on('fileBegin', function(name, file) {
+			file.path = body.uploadDir + file.name;
+		})
+	    .on('progress', function(bytesReceived, bytesExpected) {
+	        console.log('progress: ' + bytesReceived + '/' + bytesExpected);
+	    })
+	    .on('end', function() {
+	        console.log('-> upload done');
+	        
+	        for (var field in files) {
+	            resultPaths.push(files[field][1].path);
+	        }
+			        
+	        for (var field in fields) {
+	            if (fields[field][0] == "author") content.author = fields[field][1];
+	            else if (fields[field][0] == "memo") content.memo = fields[field][1];
+	        }
+	
+	        content.date = new Date();
+	        content.file = resultPaths;
+	
+	        console.log(JSON.stringify(content));
+	
+	        db.open(function(err) {
+	            if (err) throw err;
+	            collection.insert(content, function(err, data) {
+	                if (err) throw err;
+	                console.log("insert Data: ", data.result);
+	                db.close();
+	            });
+	        });
+	    });
+
+	body.parse(req, function(err, fields, files) {
+	    res.writeHead(200, { "Content-type" : "application/json" });
+		res.write(JSON.stringify(content));
+	    res.end();
+	});
+};
+
+
+exports.read = function(req, res) {
+	_findMemo({}, function(error, results) {
+			res.writeHead(200, { "Content-type" : "application/json" });
+			res.write(results);
+			res.end();
+	 });
+};
+
+exports.update = function(req, res, body) {
+	var query = url.parse(req.url).query;
+	var where = querystring.parse(query);
+	
+	_updateMemo(where, body, function(error, results) {
+		res.writeHead(200, { "Content-type" : "application/json" });
+		res.write("update memo");
+		res.end();
+	});
+};
+
+exports.remove = function(req, res, body) {
+	var query = url.parse(req.url).query;
+	var where = querystring.parse(query);
+	
+	_removeMemo(where, function(error, results) {
+		res.writeHead(200, { "Content-type" : "text/plain" });
+		res.write("remove memo");
+		res.end();
+	});
+};
+
+function _findMemo(where, callback) {
+	where = where || { };
+	
+	db.open(function(err) {
+        if (err) throw err;
+        collection.find(where).toArray(function(err, data) {
+            if (err) throw err;
+            
+            var foundData = JSON.stringify(data);
+            
+            console.log("find Data: ", foundData);
+            db.close();
+            callback(null, foundData);
+        });
+    });
+}
+
+function _updateMemo(where, body, callback) {
+	body = typeof body === "string" ? JSON.parse(body) : body;
+	
+	var operator = {$set : body};
+	var options = { multi : true };
+	
+	db.open(function(err) {
+        if (err) throw err;
+        collection.update(where, operator, options, function(err, data) {
+            if (err) throw err;
+            
+            console.log("update Data: ", data.result);
+            db.close();
+            callback(null, data);
+        });
+    });
+}
+
+function _removeMemo(where, callback) {
+	var options = { multi : true };
+	
+	db.open(function(err) {
+        if (err) throw err;
+        collection.remove(where, options, function(err, data) {
+            if (err) throw err;
+            
+            console.log("remove Data: ", data.result);
+            db.close();
+            callback(null, data);
+        });
+    });
 }
--- memo_server/server.js
+++ memo_server/server.js
@@ -1,17 +1,31 @@
-var http = require('http');
-var route = require('./route.js');
+/* server.js */
+
+var http = require("http"),
+	route = require("./route.js"),
+	url = require("url"),
+	formidable = require('formidable');
 
 function onRequest(req, res) {
-	var body = '';
-
-	req.on('data', function(chunk) {
+	var body = "";
+	var form;
+	
+	req.on("data", function(chunk) {
 		body += chunk;
 	});
+	
+	if (req.url == '/memo' && req.method.toUpperCase() == 'POST') {
+		form = new formidable.IncomingForm();
+		route.route(req, res, form);
+	}
 
-	req.on('end', function() {
-		route.route(req, res, body);
-	});
+	else {
+		req.on('end', function() {
+			route.route(req, res, body);
+		});
+	}
 }
 
 var server = http.createServer(onRequest);
 server.listen(8080);
+
+console.log("Server Start!");
